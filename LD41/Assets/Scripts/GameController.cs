8cc0cc7c7062808f5d89dc8755ba1aee3840e302
Lazlo319 <Lazlogaming@gmail.com>
2018-04-21 10:27:59 +0200

    ACTUALLY SET UP

---
+++ LD41/Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    List<Entity> initiative,entities;
+	// Use this for initialization
+	void Start () {
+        initiative = new List<Entity>();
+	}
+	void StartCombat()
+    {
+        initiative.AddRange(GameObject.FindObjectsOfType<Entity>());
+        List<Entity> list = (List<Entity>)initiative.OrderByDescending(x => x.Speed);// for 3, 2, 1
+        initiative = list; 
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
