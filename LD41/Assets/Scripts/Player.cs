078363580f6e7d22928754261a3708cc70d05ebd
Lazlo319 <Lazlogaming@gmail.com>
2018-04-22 18:46:09 +0200

    Throwing toothpicks
    Spawning pasta makes them too small as well as with no velocity, must be fixed

--- LD41/Assets/Scripts/Player.cs
+++ LD41/Assets/Scripts/Player.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public Weapon[] weapons;
     public Vector3 handPosition;//where to spawn weapon models
     public Camera viewCam;//used for spawning weapon models, they need to be children of the camera
-
-    private int currentWeapon = 0;
-    private int pastaAmmo = 0;
+    public Text ammoCounter;
+    public GameObject projectile;//the pasta javelin
+    public GameObject pastaSpawn;//where the pasta flies out of
+    private int currentWeapon = 2;
+    [SerializeField]  private int pastaAmmo = 0;
 
     public int CurrentWeapon
     {
@@ -26,15 +29,31 @@ public class Player : MonoBehaviour
 
     }
 
+
+
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            switch (currentWeapon) {
+                case 0:
+                case 1: GetComponent<Shooting>().FireShot(); break;
+                case 2: TossPasta();break;
+            }
+        }
+
+
         if (Input.GetButtonDown("Weapon 1"))
         {
             if (currentWeapon != 0)
             {
                 //spawn 1st weapon
+                weapons[0].gameObject.SetActive(true);
+                weapons[1].gameObject.SetActive(false);
+                weapons[2].gameObject.SetActive(false);
                 currentWeapon = 0;
+                ammoCounter.text = "";
             }
         }
         else
@@ -43,17 +62,60 @@ public class Player : MonoBehaviour
             if (currentWeapon != 1)
             {
                 //spawn 2nd weapon
+                weapons[0].gameObject.SetActive(false);
+                weapons[1].gameObject.SetActive(true);
+                weapons[2].gameObject.SetActive(false);
                 currentWeapon = 1;
+                ammoCounter.text = "";
             }
         }
         else
         if (Input.GetButtonDown("Weapon 3"))
         {
-            if (currentWeapon != 2)
+            if (pastaAmmo > 0)
             {
-                //spawn 3rd weapon
-                currentWeapon = 2;
+                if (currentWeapon != 2)
+                {
+                    //spawn 3rd weapon
+                    weapons[0].gameObject.SetActive(false);
+                    weapons[1].gameObject.SetActive(false);
+                    weapons[2].gameObject.SetActive(true);
+                    currentWeapon = 2;
+                    ammoCounter.text = pastaAmmo+"/50";
+                }
             }
         }
+
+        if (currentWeapon == 2)
+        {
+            if (pastaAmmo > 0)
+            {
+                ammoCounter.text = pastaAmmo+"/50";
+            } else
+            {
+                //change to first weapon
+
+                //spawn 1st weapon
+                weapons[0].gameObject.SetActive(true);
+                weapons[1].gameObject.SetActive(false);
+                weapons[2].gameObject.SetActive(false);
+                currentWeapon = 0;
+                ammoCounter.text = "";
+            }
+        }
+    }
+
+    void TossPasta()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var pasta = (GameObject)Instantiate(
+            projectile, pastaSpawn.transform.position, pastaSpawn.transform.rotation);
+
+        // Add velocity to the bullet
+        pasta.GetComponent<Rigidbody>().velocity = pasta.transform.forward * 2;
+
+        // Destroy the bullet after 4 seconds
+        //Destroy(pasta, 4.0f);
+        pastaAmmo--;
     }
 }
--- LD41/Assets/Scripts/Spawner.cs
+++ LD41/Assets/Scripts/Spawner.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour {
 
+    public GameObject entity;
+    public float interval;//how much time in seconds between each spawn
+    public bool limited = false;
+    public int limit = 50;
+    private float timer=1f;
+
 	// Use this for initialization
 	void Start () {
-		
+        timer = interval;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            if (!limited)
+            {
+                if (GameObject.FindGameObjectsWithTag("Food").Length < limit)
+                {
+                    Object.Instantiate(entity, transform.position, transform.rotation);
+                }
+            }
+            timer = interval;
+        }
 	}
 }
